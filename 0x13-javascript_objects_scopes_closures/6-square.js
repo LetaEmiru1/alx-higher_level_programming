6fd7d78b8db1b875f4a7b85cfce36d62388bd763
Leta Malimo <letaemiruleta@gmail.com>
2024-04-11 17:02:44 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
 const Square5 = require('./5-square');
+
 class Square extends Square5 {
-  charPrint(c){
-    if (c == undefined) {
+  charPrint (c) {
+    if (c === undefined) {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++){
