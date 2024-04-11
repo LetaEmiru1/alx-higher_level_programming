24f5aa4bbdc1168b5f976574eb6fcda38ab2de83
Leta Malimo <letaemiruleta@gmail.com>
2024-04-11 23:41:26 +0300

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list){
-  let temp = list;
-  for (let i = (temp.length-1); i > -1; i--){
-    temp[i] = list[i];
+  let temp = [];
+  for (let i = (list.length-1); i > -1; i--){
+    temp.push(list[i]);
   }
   return (temp);
 }
