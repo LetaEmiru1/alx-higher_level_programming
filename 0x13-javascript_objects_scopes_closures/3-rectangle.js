fb57ff54a9137e6030cd3cf799e1eefd58683cd4
Leta Malimo <letaemiruleta@gmail.com>
2024-04-11 11:37:56 +0300

    modified

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
@@ -10,9 +9,11 @@ class Rectangle {
 
   print () {
     for (let i = 0; i < this.height; i++) {
+      let s = '';
       for (let j = 0; j < this.width; j++) {
-        console.log('X');
+        s += 'X';
       }
+      console.log(s);
     }
   }
 }
