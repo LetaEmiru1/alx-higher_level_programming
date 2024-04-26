d6dfbac42a918175cb561276c9e19486bb4e4f5e
Leta Malimo <letaemiruleta@gmail.com>
2024-04-26 12:08:03 +0300

    modified

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Make a request to the server and redirect the response to /dev/null
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" -L -o /dev/null
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -L -o /dev/null
 
 # Check if the request was successful (HTTP status code 200)
 if [ $? -eq 0 ]; then
-    # If successful, display the message
-    echo "You got me!"
+    # If successful, exit with status code 0
+    exit 0
 fi
+
+# If not successful, exit with status code 1
+exit 1
 
